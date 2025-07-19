eaf01468baf22e827a17cd46b1c1762cea8178dc
Yom-Coding <yompatel88888888@gmail.com>
2025-07-19 11:43:31 +0100

    Add files via upload

---
+++ Alien.py
@@ -0,0 +1,31 @@
+import pgzrun
+from random import randint  
+
+TITLE = "SHOOT THE ALIEN"
+
+WIDTH = 500
+HEIGHT = 500
+alien = Actor("alien")
+message = ""
+
+def draw():
+    screen.clear()
+    screen.fill(color = (128, 0, 0 ))
+    alien.draw()
+    screen.draw.text(message, center = (400, 10), fontsize = 30)
+
+def place_alien():
+    alien.x = randint(50, WIDTH - 50)
+    alien.y = randint(50, HEIGHT - 50)
+
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        message = "Good Shot!"
+        place_alien()
+
+    else:
+        message = "You Missed"
+        place_alien()
+
+pgzrun.go()
